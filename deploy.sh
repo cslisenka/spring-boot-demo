c9371abc2d285bdafe8be745989c2fb3e48f0478
kslisenko <kslisenko@gmail.com>
2019-08-14 00:53:34 +0300

    Prepared step 1 - quote service

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
 pkill -f quoteService
-pkill -f streamingService
-pkill -f adminService
 
 dir=$(pwd)
 echo $dir
 rm -rf $dir/deploy/*
-
-mkdir $dir/deploy/admin
-cp $dir/admin/target/*.jar $dir/deploy/admin
-cp $dir/admin/bin/start.sh $dir/deploy/admin
-cp $dir/admin/src/main/resources/* $dir/deploy/admin
-
-mkdir $dir/deploy/streaming-service
-cp $dir/streaming-service/target/*.jar $dir/deploy/streaming-service
-cp $dir/streaming-service/bin/start.sh $dir/deploy/streaming-service
-cp $dir/streaming-service/src/main/resources/* $dir/deploy/streaming-service
 
 mkdir $dir/deploy/quote-service
 cp $dir/quote-service/target/*.jar $dir/deploy/quote-service
--- readme.md
+++ readme.md
@@ -14,20 +14,3 @@ in my case it was "172.17.0.1"
 
 1.3 Run prometheus
 docker run -p 9090:9090 -v /media/kslisenko/storage/work/spring-boot-demo/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
-
-2. Grafana
-2.1 Start
-docker run -d -p 3000:3000 grafana/grafana
-
-2.2 Login
-admin/admin
-
-3.3 Set Prometheus datasource
-
-3.4 Import dashboard
-
-TODO
-1. Bash scripts to deploy and start spring-boot applications + tune JVM parameters
-2. spring security
-3. https
-4. configure tomcat/netty logs
